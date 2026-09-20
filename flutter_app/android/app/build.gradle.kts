f7e0a78bb631e92042258e9dae4a79d247c0d096
ShanudhaTirosh <shanudhatirosh2009@outlook.com>
2026-09-20 13:07:15 +0530

    fix(ci): set compileSdk 36 override for Android plugins to resolve AAR checkReleaseAarMetadata failure

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.shanusend.shanu_send_flutter"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
